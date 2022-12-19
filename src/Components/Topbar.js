f9a017f2c4f0a689e50e1fecc6782755e27e5ec4
IswaryaAnandhan <aishwariyaanandhan@gmail.com>
2022-12-19 12:29:48 +0530

    add changes

--- src/Components/Topbar.js
+++ src/Components/Topbar.js
@@ -4,7 +4,7 @@ import { Link } from "react-router-dom";
 function Topbar() {
   return (
     <>
-      <nav class="navbar navbar-expand-lg bg-blue ">
+      <nav class="navbar navbar-expand-lg bg-primary ">
         <div class="container-fluid">
           <a class="navbar-brand text-light mx-2 fw-bold" href="#">
             URL SHORTNER APP
